8343b9abb1b8ceeab9eaf75ff1b87057150d2100
wolwolton <yusakuwithniko@gmail.com>
2020-02-14 21:54:19 +0900

    Day4(shimashima using pointer and [])

--- day4/bootpack.c
+++ day4/bootpack.c
@@ -5,10 +5,11 @@ extern void _write_mem8(int addr, int data);
 void HariMain(void)
 {
     int i;
+    char* p;
+    p = (char*)0xa0000;
 
-
-    for(i = 0xa0000; i<= 0xaffff; i++){
-        _write_mem8(i,i %0x0f);
+    for(i = 0; i<= 0xffff; i++){
+        p[i] = i&0x0f;
     }
     for(;;){
         _io_hlt();
